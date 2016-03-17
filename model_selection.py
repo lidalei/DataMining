fafbcb817b3cd60320cae6017f0e11b3992e30be
daleili <lidalei151781@gmail.com>
2016-03-17 11:32:50 +0100

    Add decisiontreeclassifier model selection.

--- model_selection.py
+++ model_selection.py
@@ -2,13 +2,12 @@ import numpy as np
 import matplotlib.pylab as plt
 from sklearn.datasets import make_blobs
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.cross_validation import KFold, cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import KFold
 from sklearn.utils import resample
-from sklearn.metrics import zero_one_loss, accuracy_score, confusion_matrix
-import itertools
-import seaborn
+from sklearn.metrics import zero_one_loss
 
-def model_selection():    
+def model_selection(clf, parameter, parameter_range): 
     fig, ax = plt.subplots(1, 1)
     
     ## generate dataset
@@ -16,10 +15,10 @@ def model_selection():
     X, y = make_blobs(n_samples = n_samples, n_features = 2, centers = n_centers, random_state = 100)
     # y = np.take([True, False], (y < 5))
     
-    k_candidates = xrange(1, 50)
     misclassify_rate_k_fold = []
-    for k in k_candidates:
-        clf = KNeighborsClassifier(n_neighbors = k)
+    for k in parameter_range:
+        para = {parameter: k}
+        clf.set_params(**para)
         scores = []
         kfold = KFold(n = X.shape[0], n_folds = 10)
         for train, test in kfold:
@@ -30,13 +29,14 @@ def model_selection():
         misclassify_rate_k_fold.append([np.mean(scores), np.std(scores)])
     
     misclassify_rate = np.array(misclassify_rate_k_fold, dtype = np.float64)
-    ax.plot(k_candidates, misclassify_rate[:, 0], label = '10-fold validation mean')
-    ax.plot(k_candidates, misclassify_rate[:, 1], label = '10-fold validation std')
-    
+    ax.plot(parameter_range, misclassify_rate[:, 0], label = '10-fold validation mean')
+    ax.plot(parameter_range, misclassify_rate[:, 1], label = '10-fold validation std')
+
     misclassify_rate_bt = []
     ## bootstraping
-    for k in k_candidates:
-        clf = KNeighborsClassifier(n_neighbors = k)
+    for k in parameter_range:
+        para = {parameter: k}
+        clf.set_params(**para)
         scores = []
         for i in xrange(0, 100):
             X_train, y_train = resample(X, y)
@@ -46,8 +46,8 @@ def model_selection():
         misclassify_rate_bt.append([np.mean(scores), np.std(scores)])
     
     misclassify_rate = np.array(misclassify_rate_bt, dtype = np.float64)
-    ax.plot(k_candidates, misclassify_rate[:, 0], label = '100 boostraping mean')
-    ax.plot(k_candidates, misclassify_rate[:, 1], label = '100 boostraping std')
+    ax.plot(parameter_range, misclassify_rate[:, 0], label = '100 boostraping mean')
+    ax.plot(parameter_range, misclassify_rate[:, 1], label = '100 boostraping std')
     
     ax.set_ylim(0.0)
     ax.set_xlabel('k')
@@ -57,4 +57,5 @@ def model_selection():
     plt.show()
 
 if __name__ == '__main__':
-    model_selection()
+    model_selection(KNeighborsClassifier(), 'n_neighbors', xrange(1, 50))
+    model_selection(DecisionTreeClassifier(), 'max_depth', xrange(3, 10))
