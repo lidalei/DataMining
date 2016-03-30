22cf733c36d9bb25af8b22860f91761e86bb7968
daleili <lidalei151781@gmail.com>
2016-03-30 18:44:51 +0200

    Optimize AUC vs gamma and C heatmap.

--- landscape_analysis.py
+++ landscape_analysis.py
@@ -28,6 +28,28 @@ def get_dataset(did):
     
     return X, y, attribute_names, target_attribute_names
 
+def grid_search_gamma(rbf_svm, X, y):
+    ## grid search - gamma only
+    # use a full grid over all parameters
+    param_grid = {'gamma': np.logspace(-15, 4, num = 5000, base = 2.0)}
+    grid_search = GridSearchCV(rbf_svm, param_grid = param_grid, scoring = 'roc_auc',
+                               cv = 10, pre_dispatch = '2*n_jobs', n_jobs = -1)
+    # re-fit on the whole training data
+    grid_search.fit(X, y)
+    grid_search_scores = [score[1] for score in grid_search.grid_scores_]
+    print('Best parameters : {}'.format(grid_search.best_params_))
+    print('Best score : {}'.format(grid_search.best_score_))
+    
+    # set canvas
+    fig, ax = plt.subplots(1, 1)
+    # ax.scatter(X[:, 0], X[:, 1], c = y)
+    ax.plot(param_grid['gamma'], grid_search_scores)
+    ax.set_title('AUC = f(gamma, C = 1.0)', fontsize = 'large')
+    ax.set_xlabel('gamma', fontsize = 'medium')
+    ax.set_ylabel('AUC', fontsize = 'medium')
+    
+    return fig    
+
 if __name__ == '__main__':    
     ## get data
     X, y, attribute_names, target_attribute_names = get_dataset(59)
@@ -35,28 +57,12 @@ if __name__ == '__main__':
     ## define classifier - rbf svm
     rbf_svm = SVC(kernel = 'rbf')
     
-    ## grid search - gamma only
-    # use a full grid over all parameters
-    param_grid = {'gamma': np.logspace(-15, 15, num = 5000, base = 2.0)}
-    grid_search = GridSearchCV(rbf_svm, param_grid = param_grid, scoring = 'roc_auc',
-                               cv = 10, pre_dispatch = '2*n_jobs', n_jobs = -1)
-    # re-fit on the whole training data
-    grid_search.fit(X, y)
-    grid_search_scores = [score[1] for score in grid_search.grid_scores_]
-    
-    # set canvas
-    fig, ax = plt.subplots(1, 1)
-    # ax.scatter(X[:, 0], X[:, 1], c = y)
-    ax.plot(param_grid['gamma'], grid_search_scores)
-    ax.set_title('gamma', fontsize = 'large')
-    ax.set_xlabel('gamma', fontsize = 'medium')
-    ax.set_ylabel('AUC', fontsize = 'medium')
-    '''
     start_time = time.time()
     ## grid search - gamma and C, grid_den = 20, time needed = 13.36s
-    grid_den = 1000
-    param_grid = {'gamma': np.logspace(-15, 15, num = grid_den, base = 2.0),
-                  'C': np.logspace(-15, 15, num = grid_den, base = 2.0)}
+    grid_den = 400
+    param_grid = {'C': np.logspace(-15, 15, num = grid_den, base = 2.0),
+                  'gamma': np.logspace(-15, 15, num = grid_den, base = 2.0)
+                  }
     grid_search = GridSearchCV(rbf_svm, param_grid = param_grid, scoring = 'roc_auc',
                                cv = 10, pre_dispatch = '2*n_jobs', n_jobs = 4)
     grid_search.fit(X, y)
@@ -69,39 +75,41 @@ if __name__ == '__main__':
     with open('scores.json', 'w+') as f:
         json.dump(scores.tolist(), f)
     f.close()
+    
     fig, ax = plt.subplots(1, 1)
     ax.set_title('AUC = f(C, gamma)', fontsize = 'large')
     # reset rc params to defaults
-    im = ax.imshow(scores, interpolation = 'none',
+    im = ax.imshow(scores, interpolation = 'none', origin = 'lower',
                    cmap = sns.light_palette("green", as_cmap = True))
     # align colormap with heatmap
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.1)
+    cax = divider.append_axes("right", size = "5%", pad = 0.2)
     plt.colorbar(mappable = im, ax = ax, cax = cax, orientation = 'vertical')
     ax.set_xlabel('gamma', fontsize = 'medium')
     ax.set_ylabel('C', fontsize = 'medium')
     
-    ax.set_xticks(np.arange(grid_den))
+    ax.set_xticks(np.arange(grid_den)[::20])
+    
     gamma_s = param_grid['gamma'].tolist()
     # save gamma_s
     with open('gamma_s.json', 'w+') as f:
         json.dump(gamma_s, f)
     f.close()
     gamma_s = ['{:0.5f}'.format(gamma) for gamma in gamma_s]
-    ax.set_xticklabels(gamma_s, rotation = 45, fontsize = 'smaller')
-    ax.set_yticks(np.arange(grid_den))
+    ax.set_xticklabels(gamma_s[::20], rotation = 270, fontsize = 'smaller')
+    ax.set_yticks(np.arange(grid_den)[::20])
+    
     C_s = param_grid['C'].tolist()
     # save C_s
     with open('C_s.json', 'w+') as f:
         json.dump(C_s, f)
     f.close()
     C_s = ['{:0.5f}'.format(C) for C in C_s]
-    ax.set_yticklabels(C_s, fontsize = 'smaller')
+    ax.set_yticklabels(C_s[::20], fontsize = 'smaller')
     ax.grid(False)
     # auto-adjust the layout
     fig.tight_layout()
     
     fig.savefig('AUC.pdf')
-    '''
     
     plt.show()
