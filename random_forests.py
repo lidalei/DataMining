605bd36b50c9d5c80601d05eccdde78a5824d360
daleili <d.li@student.tue.nl>
2016-04-16 20:21:47 +0200

    Change bias to squared bias.

--- random_forests.py
+++ random_forests.py
@@ -1,4 +1,5 @@
-import os
+import os, time
+from joblib import Parallel, delayed
 from openml.apiconnector import APIConnector
 from scipy.io.arff import loadarff
 import numpy as np
@@ -35,24 +36,25 @@ def bias_var(true_preds, sum_preds, counts, n_replicas):
     @param true_preds: true labels
     @param sum_preds: array of summation of the predictions of each sample
     @param counts: the times each sample is tested (predicted)
-    @return: bias, variance
+    @return: squared bias, variance
     '''
     sample_bias = np.absolute(true_preds - sum_preds / counts)
     sample_var = sample_bias * (1.0 - sample_bias)
     
-    weighted_sample_bias_2 = np.power(sample_bias, 2.0) * counts / n_replicas
-    weighted_sample_var = sample_var * counts / n_replicas
+    weighted_sample_bias_2 = np.power(sample_bias, 2.0) * (counts / n_replicas)
+    weighted_sample_var = sample_var * (counts / n_replicas)
     bias = np.mean(weighted_sample_bias_2)
     var = np.mean(weighted_sample_var)
     
     return bias, var
 
 def clf_bias_var(clf, X, y, n_replicas):
+        
     roc_auc_scorer = get_scorer("roc_auc")
     # roc_auc_scorer(clf, X_test, y_test)
     auc_scores = []
     error_scores = []
-    counts = np.zeros(X.shape[0], dtype = np.int64)
+    counts = np.zeros(X.shape[0], dtype = np.float64)
     sum_preds = np.zeros(X.shape[0], dtype = np.float64)
     for it in xrange(n_replicas):
         # generate train sets and test sets
@@ -67,13 +69,16 @@ def clf_bias_var(clf, X, y, n_replicas):
         
         auc_scores.append(roc_auc_scorer(clf, X[test_indices], y[test_indices]))
         error_scores.append(zero_one_loss(y[test_indices], clf.predict(X[test_indices])))
-               
+        
         preds = clf.predict(X)
         for index in test_indices:
             counts[index] += 1
             sum_preds[index] += preds[index]
     
     test_mask = (counts > 0) # indices of samples that have been tested
+    
+    # print('counts mean: {}'.format(np.mean(counts)))
+    # print('counts standard derivation: {}'.format(np.std(counts)))
     
     bias, var = bias_var(y[test_mask], sum_preds[test_mask], counts[test_mask], n_replicas)
     
@@ -83,37 +88,42 @@ if __name__ == '__main__':
     ## get dataset
     X, y, attribute_names, target_attribute_names = get_dataset(44)
     
-    ns = np.logspace(0, 7, num = 8, endpoint = True, base = 2.0, dtype = np.int32)
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ns = np.logspace(11, 0, num = 12, endpoint = True, base = 2.0, dtype = np.int32)
+    
+    fig, ax = plt.subplots(1, 1)
+    fig.suptitle('OOB error versus cross validation error', fontsize = 'x-large')
     ## OOB scores
     oob_err_rates = []
     for n in ns:
-        rnd_forest_clf = RandomForestClassifier(n_estimators = n, bootstrap = True, oob_score = True, warm_start = True, random_state = 100)
+        rnd_forest_clf = RandomForestClassifier(n_estimators = n, bootstrap = True, oob_score = True)
         rnd_forest_clf.fit(X, y)
         oob_err_rates.append(1.0 - rnd_forest_clf.oob_score_)
         # plot_surface(ax, rnd_forest_clf, X, y)
-    ax1.plot(ns, oob_err_rates, label = 'OOB error')
-    
-    ax1.legend(loc = 'best', fontsize = 'medium')
-    ax1.set_xlabel('Number of base classifiers')
+    ax.plot(ns, oob_err_rates, '-o', label = 'OOB error')
     
     ## cross validation scores
     cv_err_rates = []
     for n in ns:
-        rnd_forest_clf = RandomForestClassifier(n_estimators = n, bootstrap = True, oob_score = False, warm_start = True, random_state = 100)
+        rnd_forest_clf = RandomForestClassifier(n_estimators = n, bootstrap = True, oob_score = False)
         scores = cross_val_score(rnd_forest_clf, X, y, cv = 10, n_jobs = -1)
         cv_err_rates.append([1.0 - np.mean(scores), np.std(scores)])
         # plot_surface(ax, rnd_forest_clf, X, y)
     cv_err_rates = np.array(cv_err_rates)
-    ax2.plot(ns, cv_err_rates[:, 0], label = 'CV error mean')
-    ax2.plot(ns, cv_err_rates[:, 1], label = 'CV error std')
+    ax.plot(ns, cv_err_rates[:, 0], '-o', label = 'Cross validation error')
+    # ax.plot(ns, cv_err_rates[:, 1], label = 'CV error std')
     
-    ax2.legend(loc = 'best', fontsize = 'medium')
-    ax2.set_xlabel('Number of base classifiers')
+    ax.grid(True)
+    ax.legend(loc = 'best', fontsize = 'large')
+    ax.set_xlabel('Number of trees', fontsize = 'large')
+    ax.set_ylabel('Error rate', fontsize = 'large')
+    ax.set_xlim(np.min(ns) - 1, np.max(ns) + 4)
+    
     
     ## compare a single tree with RandomForest ensemble, using 100 bootstrap
-    figure, ax = plt.subplots(1, 1)
-    n_replicas = 100
+    figure, (ax1, ax2) = plt.subplots(2, 1)
+    n_replicas = 200
+    
+    
     # compute bias and variance for a tree
     cart = DecisionTreeClassifier()
     auc_scores, error_scores, bias, var = clf_bias_var(cart, X, y, n_replicas)
@@ -121,24 +131,41 @@ if __name__ == '__main__':
     print('error mean: {}, std: {}'.format(np.mean(error_scores), np.std(error_scores)))
     print('bias: {}, var: {}'.format(bias, var))
     
-    #ax.plot(ns[[0, -1]], [bias, bias], '--', label = 'CART bias')
-    ax.plot(ns[[0, -1]], [var, var], '--', label = 'CART variance')
+    # ax1.plot(ns[[0, -1]], [bias, bias], '--', label = 'CART bias')
+    # ax1.plot(ns[[0, -1]], [var, var], '--', label = 'CART variance')
     
+    
+    aucs = []
+    err_rates = []
     biases_vars = []
-    rnd_forest_clf = RandomForestClassifier(bootstrap = True, oob_score = False, warm_start = True, random_state = 100)
-    for n in ns:
-        rnd_forest_clf.set_params(**{'n_estimators': n})
-        auc_scores, error_scores, bias, var = clf_bias_var(rnd_forest_clf, X, y, n_replicas)
+    
+    start_time = time.time()
+    results = Parallel(n_jobs = 8)(delayed(clf_bias_var)(RandomForestClassifier(n_estimators = n, bootstrap = True, oob_score = False),
+                                                         X, y, n_replicas) for n in ns)
+    print('Time: {}'.format(time.time() - start_time))
+    for auc_scores, error_scores, bias, var in results:
         print('auc mean: {}, std: {}'.format(np.mean(auc_scores), np.std(auc_scores)))
         print('error mean: {}, std: {}'.format(np.mean(error_scores), np.std(error_scores)))
-        print('bias: {}, var: {}'.format(bias, var))
+        print('squared bias: {}, var: {}'.format(bias, var))
+        aucs.append(np.mean(auc_scores))
+        err_rates.append(np.mean(error_scores))
         biases_vars.append([bias, var])
     
     biases_vars = np.array(biases_vars)
     
-    #ax.plot(ns, biases_vars[:, 0], label = 'Random forest bias')
-    ax.plot(ns, biases_vars[:, 1], label = 'Random forest variance')
+    ax1.plot(ns, aucs, 'o-', label = 'Random Forest AUC scores')
+    ax1.legend(loc = 'best', fontsize = 'medium')
+    ax1.set_xlabel('Number of trees', fontsize = 'medium')
+    ax1.set_xlim(np.min(ns) - 1, np.max(ns) + 4)
+    ax1.grid(True, which = 'both')
     
-    ax.legend(loc = 'best', fontsize = 'medium')
+    ax2.plot(ns, err_rates, 'o-', label = 'Random Forest error rate')
+    ax2.plot(ns, biases_vars[:, 0], 'o-', label = 'Random forest squared bias')
+    ax2.plot(ns, biases_vars[:, 1], 'o-', label = 'Random forest variance')
+    ax2.legend(loc = 'best', fontsize = 'medium')
+    ax2.set_xlabel('Number of trees', fontsize = 'medium')
+    ax2.set_xlim(np.min(ns) - 1, np.max(ns) + 4)
+    ax2.grid(True, which = 'both')
     
+    plt.tight_layout()
     plt.show()
