418af5c8102f2b4a884a4ab442f952ee4b378671
Dalei Li <lidalei151781@gmail.com>
2016-04-17 20:21:10 +0200

    Add SGDDataset to implement next_batch method which is similar to tensorflow.

---
+++ SGDDataset.py
@@ -0,0 +1,55 @@
+"""Functions for downloading and reading MNIST data."""
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+'''
+Adapted from tensorflow.contrib.learn.python.learn.datasets.mnist.py
+Dalei Li, lidalei151781@gmail.com
+'''
+import numpy
+import tensorflow as tf
+
+class SGDDataSet(object):
+
+    def __init__(self, images, labels, dtype=tf.float32):
+        dtype = tf.as_dtype(dtype).base_dtype
+        
+        if dtype is not tf.float32:
+            raise TypeError('Invalid image dtype %r, expected float32' %dtype)
+        assert images.shape[0] == labels.shape[0], ('images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
+        
+        self._num_examples = images.shape[0]
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+    
+    def next_batch(self, batch_size):
+        """Return the next `batch_size` examples from this data set."""
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+            perm = numpy.arange(self._num_examples)
+            numpy.random.shuffle(perm)
+            self._images = self._images[perm]
+            self._labels = self._labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+        return self._images[start:end], self._labels[start:end]
