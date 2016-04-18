99616ff7ef091a1466d1601cf73c673f58db9c72
Dalei Li <lidalei151781@gmail.com>
2016-04-18 22:35:42 +0200

    Ignore the initial SGD step.

--- visualize_nn_train_process.py
+++ visualize_nn_train_process.py
@@ -1,21 +1,23 @@
 import matplotlib.pylab as plt
 import json
 
-fig, ax2 = plt.subplots(1, 1)
+fig1, ax1 = plt.subplots(1, 1)
+fig2, ax2 = plt.subplots(1, 1)
 
 for learning_rate in [0.0001, 0.001, 0.01, 0.1]:
     with open('train_process_learning_rate_' + str(learning_rate) + '.json', 'r') as f:
         train_process = json.load(f)
     f.close()
 
-    it_counts = train_process['it_counts']
+    it_counts = train_process['it_counts'][1:]
     
-    # ax1.plot(it_counts[1:], train_process['loss_values'][1:], label = 'Loss')
-    # ax1.plot(it_counts[1:], train_process_alpha['loss_values'][1:], label = 'Loss with l2 regularization')
-    # ax1.legend(loc = 'best', fontsize = 'medium')
+    ax1.plot(it_counts, train_process['loss_values'][1:], label = 'Loss')
     
-    ax2.plot(it_counts, train_process['train_scores'], label = 'Training accuracy - learning rate = ' + learning_rate)
-    ax2.plot(it_counts, train_process['test_scores'], label = 'Test accuracy - learning rate = ' + learning_rate)
+    ax2.plot(it_counts, train_process['train_scores'][1:], label = 'Training accuracy - learning rate = ' + learning_rate)
+    ax2.plot(it_counts, train_process['test_scores'][1:], label = 'Test accuracy - learning rate = ' + learning_rate)
+
+ax1.legend(loc = 'best', fontsize = 'medium')
+ax1.grid(True)
 
 ax2.legend(loc = 'best', fontsize = 'medium')
 ax2.grid(True)
